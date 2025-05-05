6fc77f3db778b1a5c973ebf9a5b29ad91624725b
hanad <hanad.ali@ucdenver.edu>
2025-05-05 03:21:06 -0600

    edited name of js file

---
+++ helloworld.js
@@ -0,0 +1,52 @@
+/*
+const myNumber = 3;
+const myString = "Hello, World!";
+const myBoolean = true;
+
+console.log("myNumber is equal to " + myNumber);
+console.log("myString is equal to " + myString);
+console.log("myBoolean is equal to " + myBoolean);
+
+var myArray = [1, 2, 3];
+var theSameArray = new Array(1,2,3);
+
+console.log(myArray[1]);
+
+theSameArray[2] = 4;
+console.log(theSameArray);
+
+const myArray2 = ["What is the meaning of life?", 40, true];
+
+console.log(myArray2);
+
+var myStack = [];
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+console.log(myStack);
+
+var splicingArray = [0,1,2,3,4,5,6,7,8,9];
+var spliced = splicingArray.splice(3,5);
+
+console.log(spliced);
+console.log(splicingArray);
+*/
+
+var a = 1;
+var b = 2;
+var c = a + b;
+var firstName = "John";
+console.log("Hello " + firstName + "!");
+console.log("The meaning of life is " + 42);
+console.log(42 + " is the meaning of life");
+
+console.log(3 - 5);
+console.log(3 * 5);
+console.log(3 / 5);
+
+if (confirm("Are you John Smith"))
+{
+    console.log("Hello John, how are you?");
+} else {
+    console.log("Then what is your name?");
+}
